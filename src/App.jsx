f0ce3ec972430b526fbdd340af3e1c7cf76b2d85
Devika <gubbaladevika65@gmail.com>
2026-03-02 21:55:28 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Login from './pages/Login'
 import Signup from './pages/Signup'
 import { Routes,Route } from 'react-router-dom'
 import Product from './pages/Product'
-import ProductDetails from './pages/productdetails'
+import ProductDetails from './pages/Productdetails'
 
 
 const App = () => {
